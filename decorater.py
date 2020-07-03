de1bf30386e6ae968d99ce75a83b1f8abc5ef94c
Sandy <sochyi5322@gmail.com>
2020-07-04 00:12:00 +0800

    :dog:Update File:tada:

--- decorater.py
+++ decorater.py
@@ -82,3 +82,28 @@ if __name__ == "__main__":
     Dog()
     Cat()
 
+print('----------------------------------------------------------')
+print()
+
+class Dog:
+    def __init__(self, func):
+        self.age = 10
+        self.talent = func
+
+    def bark(self):
+        print("Bark !!!")
+
+@Dog
+def dog_can_pee():
+    print("I can pee very hard......")
+
+
+
+if __name__ == "__main__":
+    dog = dog_can_pee
+
+    print(dog.age)
+
+    dog.bark()
+
+    dog.talent()
